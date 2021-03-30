e0127ec284cdd7711443543fae7127fcd4c7a0e9
Emilio Gaines <emiliogaiines@gmail.com>
2021-03-30 23:01:43 +0200

    Updated knowledge layout and favicon

--- src/Components/Sections/Knowledge/Knowledge.jsx
+++ src/Components/Sections/Knowledge/Knowledge.jsx
@@ -8,14 +8,32 @@ class Showcase extends Component {
           <h5 className="card-title">🧠 Kunskaper</h5>
           <hr />
           <p className="card-text">
-            Jag har haft programmering som hobby i ungefär 8 år även om det har
-            varit lite till och från. Under denna tid har jag jobbat framförallt
-            inom följande programmeringspråk dock främst apputveckling och
-            backend:
+            Jag har haft programmering som hobby i ungefär 8 år även om det har varit lite till och från. Under denna tid har jag jobbat framförallt
+            inom följande programmeringspråk dock främst apputveckling och backend.
             <br />
             <b>
-              Kotlin, Java, Swift, .NET, C++, C#, PHP (+ SQL), React,
-              Javascript, HTML, CSS
+              • React
+              <br />
+              • React Native
+              <br />
+              • Kotlin
+              <br />
+              • Java
+              <br />
+              • Swift
+              <br />
+              • .NET
+              <br />
+              • C++
+              <br />
+              • C#
+              <br />
+              • PHP (+ SQL)
+              <br />
+              • Javascript
+              <br />
+              • HTML
+              <br />• CSS
             </b>
           </p>
         </div>
